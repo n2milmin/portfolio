473ed693eb8abab9ad595b0c5fb366f3f9b66e17
n2milmin <nicoleamilmine@gmail.com>
2025-03-10 17:53:24 -0400

    home.js src fixed. No more runtime error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from "./pages/Home.js";
+import Home from "./pages/home.js";
 import NavBar from './components/navBar.js';
 import Footer from './components/footer.js';
 
