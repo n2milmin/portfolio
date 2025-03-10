c5ffaedb42589d2f36cf4df4c27c96f3d384fdc0
n2milmin <nicoleamilmine@gmail.com>
2025-03-10 17:59:00 -0400

    Added more areas to fill in in professional

--- src/pages/professional.js
+++ src/pages/professional.js
@@ -4,18 +4,28 @@ const Professional = () => {
     return (
         <div className="professional">
             <div className="samples">
+                <div className="fitting">
 
+                </div>
+                <div className="mobile">
+
+                </div>
             </div>
             <div className="volunteer">
-
+                <h2>Volunteer Experience</h2>
+                <div className="remax">
+                    good for networking with a wide range of people 
+                </div>
             </div>
             <div className="recommendations">
+                <h2>Recommendation Letters</h2>
                 <iframe 
-                    src=""
+                    src="https://drive.google.com/file/d/1H_-gJKfoQ6UsrT7a7cPdlX34lH-W7vsJ/view?usp=sharing"
                 />
+                <p>There is a second one but I was unable to get it off a broken phone on time.</p>
             </div>
             <div className="awards">
-
+                {/* Smart Serve ??? // Dad's for diploma */}
             </div>
         </div>
     )
