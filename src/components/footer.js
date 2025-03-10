2ebbd9b3cb796e1ccff236e7e692f2475469d898
n2milmin <nicoleamilmine@gmail.com>
2025-03-10 17:49:16 -0400

    updated footer link to go to github

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,11 @@
 import './footer.css';
-import { Link } from "react-router-dom";
 
 const footer = () => {
     return(
         <div className="footer" >
-            <Link className='me' to={{ pathname: "https://github.com/n2milmin"}} target="_blank">
+            <a className='me' href="https://github.com/n2milmin" target="_blank">
                 &copy;Nicole Milmine
-            </Link>
+            </a>
         </div>
     )
 }
