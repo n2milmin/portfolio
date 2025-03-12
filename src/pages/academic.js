5b454e6ef89d3c60538c7ec4116a65a4331f4809
n2milmin <nicoleamilmine@gmail.com>
2025-03-12 18:20:11 -0400

    added headers

--- src/pages/academic.js
+++ src/pages/academic.js
@@ -4,25 +4,29 @@ const Academic = () => {
     return (
         <div className="academic">
             <div className="degrees">
-
+                <h1>Educatiion</h1>
             </div>
             <div className="awards">
-
+                <h1>Awards</h1>
             </div>
             <div className="work">
+                <h1>Projects</h1>
                 <div className="capstone">
+                    <h2>Capstone</h2>
                     <iframe 
                         src=""
                         
                     />
                 </div>
                 <div className="android">
+                    <h2>Kome Android</h2>
                     <iframe 
                         src=""
                         
                     />
                 </div>
                 <div className="apple">
+                    <h2>Kome Mobile</h2>
                     <iframe 
                         src=""
                         
@@ -32,3 +36,5 @@ const Academic = () => {
         </div>
     )
 }
+
+export default Academic;
