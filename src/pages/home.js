5d0dd4ec5da72d4b5c3021e7616deec27e76c632
n2milmin <nicoleamilmine@gmail.com>
2025-03-12 13:26:16 -0400

    Personal philosphy & bio added to home pg

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,6 +4,7 @@ import coverletter from "../files/CoverLetter.pdf"
 
 const Home = () => {
 
+    // Handle clicking on personal project photo or text 
     const handleGithub = () => {
         try {
             window.open(
@@ -22,13 +23,15 @@ const Home = () => {
             <div className="philosophy">
                 <h2>Personal Philosophy</h2>
                 <p>
-                    contribute to innovative projects, by adding uqine expirences, such as …., and understanding, simplfy and allowing them to reach wider audiances
+                    Continuous learning and embracing challenges as opportunities for growth drive success.
                 </p>
             </div>
             <div className="bio">
                 <h2>Bio</h2>
                 <p>
-                    I like to knit and walk at the beach
+                    I am a dedicated professional with a diverse background in both academia and the tech industry, currently completing a Computer Programming and Analysis diploma at George Brown College, with graduation expected in April 2025. In addition to my academic studies, I have gained hands-on experience as a remote AI Trainer at Outlier, where I focused on developing and evaluating image- and text-based machine learning models. This role involved creating and testing prompts to assess the models' ability to meet their intended requirements. My practical experience in AI, combined with ongoing coursework in machine learning, has deepened my understanding of artificial intelligence and its potential to shape future technologies.
+
+                    Previously, I spent over a year as a Software Quality Assurance Analyst Co-op at Unitron, a Sonova brand, where I developed expertise in regression testing, release testing, and bug investigation for both mobile and software applications. This role honed my attention to detail and my ability to identify and resolve issues in software systems. Outside of work, I enjoy knitting as a creative outlet and spending time with my two cats. I am passionate about problem-solving and continuous improvement and look forward to contributing my technical skills and collaborative mindset to meaningful projects, all while staying at the forefront of emerging technologies.
                 </p>
             </div>
             <div className="project">
