b8f77511685765cc36f170fca568db465f833dee
n2milmin <nicoleamilmine@gmail.com>
2025-03-13 13:18:20 -0400

    Fixed up a hover colour & footer links so now linkedin and github

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,8 +3,11 @@ import './footer.css';
 const footer = () => {
     return(
         <div className="footer" >
-            <a className='me' href="https://github.com/n2milmin" target="_blank">
+            <a href='https://www.linkedin.com/in/n2milmin/' target='_blank'>
                 &copy;Nicole Milmine
+            </a>
+            <a className='me' href="https://github.com/n2milmin" target="_blank">
+                GitHub
             </a>
         </div>
     )
